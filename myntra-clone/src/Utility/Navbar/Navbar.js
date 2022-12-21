130534dd9809c14ee9544b9ba1c68284b98ff8df
k-krutik <krutikkhandare2015@gmail.com>
2022-12-21 18:52:01 +0530

    navbar done

---
+++ myntra-clone/src/Utility/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Navbar.css"
+import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
+function Navbar() {
+    return (
+        <>
+        <nav className='nav'>
+          <div className="left">
+            <img src="./Images/logo.png" alt="" />
+            <ul>
+              <li>Men</li>
+              <li>Women</li>
+              <li>Kids</li>
+              <li>Home & Lifestyle</li>
+              <li>Beauty</li>
+              <li>Studio</li>
+            </ul>
+
+          </div>
+
+          <div className="right">
+          <div class="search">
+              <i class="fa fa-search"></i>
+              <input type="text" class="form-control" placeholder="Search for products,brands and more"/>
+          </div>
+            
+            <ul>
+              <li><PermIdentityOutlinedIcon/></li>
+              <li><FavoriteBorderOutlinedIcon/></li>
+              <li><LocalMallOutlinedIcon/></li>
+            </ul>
+          
+
+
+          </div>
+        </nav>
+       
+
+        </>
+    )
+}
+
+export default Navbar
